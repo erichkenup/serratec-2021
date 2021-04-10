11daccfdabdab4bd5dc926c8c5650f5b843dfc5e
erichkenup <erich_kenupp@hotmail.com>
2021-04-10 19:58:25 -0300

    Add files via upload

---
+++ logica_de_programacao/lista 4/fase_4/operadores_logicos/ex_14.js
@@ -0,0 +1,16 @@
+/**
+ * 14.	É considerado “Emancipado” qualquer pessoa a partir de 21 anos, 
+ * ou o indivíduo do sexo masculino a partir dos 18 anos. 
+ * Crie um programa para atestar a emancipação do cidadão segundo o ano do seu nascimento.
+ */
+
+ const ler = require("prompt-sync")();
+
+ 
+ idade = ler("Digite o ano em que você nasceu: ");
+ sexo = ler("Digite M ou F para o seu sexo: ");
+
+ var resultado = (((2021 - idade) >= 21) || ((sexo == "M")&&((2021 - idade)>=18)) );
+
+ console.log("Sexo: " + sexo + "\nAno de nascimento: " + idade +
+  "\nCidadão emancipado? " + resultado);
